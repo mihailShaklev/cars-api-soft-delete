7541807be4c113a8b0f7529511ab7fc1981a991c
Mihail Shaklev <60180238+mihailShaklev@users.noreply.github.com>
2022-01-25 16:38:28 +0200

    Update settings.py

--- cars/settings.py
+++ cars/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
